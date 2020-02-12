22af6461f3fc282fbc684cb120b02a31d198df30
Patrick Ding <pd_35_delancy@yahoo.com>
2020-02-12 17:22:41 -0600

    got exact sampler working

--- truncated_gaussian_runtime.R
+++ truncated_gaussian_runtime.R
@@ -1,10 +1,14 @@
 library(tmg)
 library(TruncatedNormal)
+library(cdists)
 library(tictoc)
+
+
+# Identity covariance
 
 # d = 100 ----
 n <- 1000
-d <- 3000
+d <- 1000
 mu <- rep(0, d)
 Sigma <- diag(d)
 lb <- rep(0, d)
@@ -13,6 +17,12 @@ ub <- rep(Inf, d)
 tic()
 botev_samples <- TruncatedNormal::rtmvnorm(n, mu, Sigma, lb, ub)
 botev_time <- toc()
+botev_ess <- coda::effectiveSize(botev_samples)
+
+tic()
+exact_samples <- cdists::rtmvn(n, Sigma, mu, lb, ub)
+exact_time <- toc()
+exact_ess <- coda::effectiveSize(exact_samples)
 
 init <- rep(1, d)
 f <- diag(d)
@@ -21,3 +31,4 @@ tic()
 pp_samples <- tmg::rtmg(n, Sigma, mu, init, f, g)
 toc()
 
+# d = 1000 ----
